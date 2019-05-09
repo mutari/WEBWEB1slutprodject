0886bbb8b178e39a5a03517c337658028e4acbd6
Philip Johansson <philipjohansson@Philips-MacBook-Pro-5.local>
2019-05-09 09:36:40 +0200

    uppdate

--- javascript/BuffeList.js
+++ javascript/BuffeList.js
@@ -1,40 +1,29 @@
 var BuffeList = 
-            [ 
-                {
-                    color: "green",
-                    name: "Grill Buffe", 
-                    about: "Denna buffe består av blandane grillade rätter.",
-                    have: "kykling spätt lätt grilade<br>oxfele(väljs själv hur grilad)<br>potatis gratäng<br>bea", 
-                    prisePers: 220,
-                    maxPers: 25,
-                    id: 1
-                },
-                {
-                    color: "blue",
-                    name: "Vego Buffe", 
-                    about: "Denna buffen är perfekt för vegitarianen.",
-                    have: "grönsaker<br>falafel<br>bönsalad<br>vock<br>", 
-                    prisePers: 150,
-                    maxPers: 40,
-                    id: 2
-                },
-                {
-                    color: "red",
-                    name: "Frukost buffe", 
-                    about: "Denna buffen är perfekt för vegitarianen.",
-                    have: "grönsaker<br>falafel<br>bönsalad<br>vock<br>", 
-                    prisePers: 150,
-                    maxPers: 40,
-                    id: 3
-                }
-            ];
+[
+    {
+        color: "green",
+        name: "Grill Buffe", 
+        about: "Denna buffe består av blandane grillade rätter.",
+        have: "kykling spätt lätt grilade<br>oxfele(väljs själv hur grilad)<br>potatis gratäng<br>bea", 
+        prisePers: 220,
+        id: 1
+    },
+    {
+        color: "blue",
+        name: "Vego Buffe", 
+        about: "Denna buffen är perfekt för vegitarianen.",
+        have: "grönsaker<br>falafel<br>bönsalad<br>vock<br>", 
+        prisePers: 150,
+        id: 2
+    },
+    {
+        color: "red",
+        name: "Frukost buffe", 
+        about: "Denna buffen är perfekt för vegitarianen.",
+        have: "grönsaker<br>falafel<br>bönsalad<br>vock<br>", 
+        prisePers: 150,
+        id: 3
+    }
+];
 
-
-
-
-
-
-
-
-
-var get = (id) => { return BuffeList.filter(e => { return e.id === id; })[0]; }
+var getBuffe = (id) => { return BuffeList.filter(e => { return e.id === id; })[0]; }
--- javascript/order.js
+++ javascript/order.js
@@ -1,5 +1,32 @@
+var order = [];
+
+function addOrder(id, type) {
+
+    var antal = document.getElementsByClassName("numberInput")[id-1].value;
+    var OBJ;
+
+    if(type == 1) {
+        OBJ = getBuffe(id);
+    }
+
+    orders = JSON.parse(window.localStorage.getItem("Orders")) == null ? [] : JSON.parse(window.localStorage.getItem("Orders"));
+
+    var order = {
+        id: orders.length,
+        antalPers: antal,
+        prise: OBJ.prisePers,
+        totalPrise: (OBJ.prisePers * antal),
+        name: OBJ.name
+    };
+
+    orders.push(order);
+    window.localStorage.setItem("Orders", JSON.stringify(orders));
+
+    alert(OBJ.name + " added to cart");
+}
+
 function order(id) {
-    window.localStorage.setItem("ActiveOBJ", JSON.stringify(get(id)));
+    window.localStorage.setItem("ActiveOBJ", JSON.stringify(getBuffe(id)));
     window.location.href = window.location.href.substr(0, window.location.href.length-9) + "/Order.html";
 }
 
